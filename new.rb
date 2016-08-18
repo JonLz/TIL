e6d604ad09743bcf1553d4942fc3b511712fa4e1
Jon Lazar <jon.lazar@gmail.com>
2016-08-18 11:27:28 -0400

    New git command. Ruby script for creating new files. Reorganize files.

---
+++ new.rb
@@ -0,0 +1,37 @@
+directory = ARGV[0]
+directories = Dir.glob('*').select {|f| File.directory? f}
+
+unless ARGV.length > 0
+	puts 'Use: new.rb <directory> <title of new file>'
+	exit
+end
+
+unless directories.include? directory
+	puts 'Invalid directory. Possible directories are:'
+	puts directories
+	exit
+end
+
+title = ARGV.drop(1)
+
+unless title.length > 0
+	puts 'Please provide a title and try again'
+	exit
+end
+
+filename = title.join('-')
+full_path = directory + '/' + filename
+
+if File.file?(full_path)
+	puts 'File already exists, bailing'
+	exit
+end
+
+header = title.join(' ').capitalize!
+
+File.open(full_path, "w") {|f| 
+	f.puts "# " + header
+	f.puts()
+}
+
+%x( subl #{full_path} )
